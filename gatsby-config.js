d85f4df84fa3483a7b86dcb57ecc4ee716ec7c4a
Tony Powell <adpowell@mtu.edu>
2019-01-27 13:04:00 -0500

    Adjust project properties

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,8 +1,8 @@
 module.exports = {
   siteMetadata: {
-    title: `Gatsby Default Starter`,
-    description: `Kick off your next, great Gatsby project with this default starter. This barebones starter ships with the main Gatsby configuration files you might need.`,
-    author: `@gatsbyjs`,
+    title: `Gatsby Wiki Template`,
+    description: `Markdown based wiki site`,
+    author: `@cephalization`,
   },
   plugins: [
     `gatsby-plugin-react-helmet`,
