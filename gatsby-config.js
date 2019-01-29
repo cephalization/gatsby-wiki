d7972d2955bb312b97f4781044c4b8de8f973d93
Anthony Powell <adpowell@mtu.edu>
2019-01-28 19:07:57 -0500

    Implement gatsby offline for PWA/offline wiki support

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,3 +1,10 @@
+const config = {
+  start_url: `/`,
+  background_color: `#663399`,
+  theme_color: `#663399`,
+  icon: `src/images/gatsby-icon.png`, // This path is relative to the root of the site.
+};
+
 module.exports = {
   siteMetadata: {
     title: `Gatsby Wiki Template`,
@@ -28,15 +35,27 @@ module.exports = {
       options: {
         name: `gatsby-starter-default`,
         short_name: `starter`,
-        start_url: `/`,
-        background_color: `#663399`,
-        theme_color: `#663399`,
+        start_url: config.start_url,
+        background_color: config.background_color,
+        theme_color: config.theme_color,
+        icon: config.icon,
         display: `minimal-ui`,
-        icon: `src/images/gatsby-icon.png`, // This path is relative to the root of the site.
       },
     },
     // this (optional) plugin enables Progressive Web App + Offline functionality
     // To learn more, visit: https://gatsby.app/offline
-    // 'gatsby-plugin-offline',
+    {
+      resolve: 'gatsby-plugin-manifest',
+      options: {
+        name: 'gatsby-wiki-template',
+        short_name: 'gatsby-wiki',
+        start_url: config.start_url,
+        background_color: config.background_color,
+        theme_color: config.theme_color,
+        icon: config.icon,
+        display: `standalone`,
+      },
+    },
+    'gatsby-plugin-offline',
   ],
 };
